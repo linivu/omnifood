ce49ea2697f031480a119ecc3c9ffed4f87c287c
Ivan Kleynhans <info@linivu.com>
2021-08-12 15:27:25 +0200

    Add LandingPage component with Home component imported

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+// COMPONENTS //
+import Home from '../components/page/Home';
+
+const LandingPage = () => {
+  return (
+    <main className="main">
+      <Home />
+    </main>
+  );
+};
+
+export default LandingPage;
