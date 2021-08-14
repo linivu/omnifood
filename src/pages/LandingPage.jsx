464b7990b62f38974e72b7cb6e3f4406c31a6af0
Ivan Kleynhans <info@linivu.com>
2021-08-14 09:40:24 +0200

    Update LandingPage component: add Header component

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,11 @@
 // COMPONENTS //
+import Header from '../components/layout/Header';
 import Home from '../components/page/Home';
 
 const LandingPage = () => {
   return (
     <main className="main">
+      <Header />
       <Home />
     </main>
   );
