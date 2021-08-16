50372a5428000d526927c0cfd6f1f1f227c33b12
Ivan Kleynhans <info@linivu.com>
2021-08-16 09:23:42 +0200

    Add initial HowItWorks component

---
+++ src/components/page/HowItWorks.jsx
@@ -0,0 +1,15 @@
+// STYLES //
+import './HowItWorks.scss';
+
+const HowItWorks = () => {
+  return (
+    <section id="how-it-works" className="how-it-works">
+      <div className="container">
+        <span className="subheading">How It Works</span>
+        <h2 className="section-heading">Your daily dose of health in 3 simple steps</h2>
+      </div>
+    </section>
+  );
+};
+
+export default HowItWorks;
