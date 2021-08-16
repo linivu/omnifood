18150fc2a773868366b6392a731c4dcbd9d1cef7
Ivan Kleynhans <info@linivu.com>
2021-08-16 09:06:28 +0200

    Update LandingPage component: add FeaturedIn component

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,12 +1,14 @@
 // COMPONENTS //
 import Header from '../components/layout/Header';
 import Home from '../components/page/Home';
+import FeaturedIn from '../components/page/FeaturedIn';
 
 const LandingPage = () => {
   return (
     <main className="main">
       <Header />
       <Home />
+      <FeaturedIn />
     </main>
   );
 };
