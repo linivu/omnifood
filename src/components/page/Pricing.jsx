98dd1a20a0619752238240ba699bc2b59862653f
Ivan Kleynhans <info@linivu.com>
2021-08-17 15:53:57 +0200

    Update Pricing component: add meals features all

--- src/components/page/Pricing.jsx
+++ src/components/page/Pricing.jsx
@@ -75,13 +75,55 @@ const Pricing = () => {
             </Button>
           </div>
         </div>
-      </div>
-      <div className="container">
+
         <div className="pricing__features">
           <p className="pricing__features-text">
             Prices include all applicable taxes. You can cancel at any time. Both plans
             include the following:
           </p>
+          <div className="grid">
+            <div className="pricing__features-feature">
+              <div className="icon">
+                <ion-icon name="infinite-outline"></ion-icon>
+              </div>
+              <h3 className="heading">Never cook again!</h3>
+              <p className="text">
+                Our subscriptions cover 365 days per year, even including major holidays.
+              </p>
+            </div>
+
+            <div className="pricing__features-feature">
+              <div className="icon">
+                <ion-icon name="nutrition-outline"></ion-icon>
+              </div>
+              <h3 className="heading">Local and organic</h3>
+              <p className="text">
+                Our cooks only use local, fresh, and organic products to prepare your
+                meals.
+              </p>
+            </div>
+
+            <div className="pricing__features-feature">
+              <div className="icon">
+                <ion-icon name="leaf-outline"></ion-icon>
+              </div>
+              <h3 className="heading">No waste</h3>
+              <p className="text">
+                All our partners only use reusable containers to package all your meals.
+              </p>
+            </div>
+
+            <div className="pricing__features-feature">
+              <div className="icon">
+                <ion-icon name="pause-outline"></ion-icon>
+              </div>
+              <h3 className="heading">Pause anytime</h3>
+              <p className="text">
+                Going on vacation? Just pause your subscription, and we refund unused
+                days.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
